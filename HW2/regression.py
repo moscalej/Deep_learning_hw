cd78317b47ba5acc1c46734f6e1575422bfee5cd
Zachary Bamberger <zeb3@cornell.edu>
2018-05-20 16:47:44 +0300

    regression working

--- HW2/regression.py
+++ HW2/regression.py
@@ -29,11 +29,13 @@ def create_data_sets():
     small_vals = []
     for x in small:
         small_vals.append(func(x[0], x[1]))
+    small_vals = np.array(small_vals).reshape([100, 1])
 
     big = make_points(1000)
     big_vals = []
     for x in big:
         big_vals.append(func(x[0], x[1]))
+    big_vals = np.array(big_vals).reshape([1000, 1])
 
     test_set = np.linspace(-2, 2, 1000)
     test = []
@@ -42,6 +44,7 @@ def create_data_sets():
         for y in range(1000):
             test.append(np.array([test_set[x], test_set[y]]))
             test_vals.append(func(test_set[x], test_set[y]))
+    test_vals = np.array(test_vals).reshape([len(test_vals), 1])
 
     return [small, small_vals, big, big_vals, test, test_vals]
 
@@ -49,8 +52,10 @@ def create_data_sets():
 if __name__ == "__main__":
 
     small, small_vals, big, big_vals, test, test_vals = create_data_sets()
-    print(small.shape)
-    print(big.shape)
+    # print(small.shape)
+    # print(big.shape)
+    print(small_vals.shape)
+    print(big_vals.shape)
 
     small_layers = [generate_layer(2, 100, "relu", "l2", 0.4)]
     small_net = mydnn.MyDNN(small_layers, "MSE")
@@ -58,7 +63,7 @@ if __name__ == "__main__":
 
     big_layers = [generate_layer(2, 1000, "relu", "l2", 0.4)]
     big_net = mydnn.MyDNN(big_layers, "MSE")
-    big_net.fit(big, big_vals, 2000, 500, 0.4)
+    big_net.fit(big, big_vals, 200, 500, 0.4)
 
     small_results = small_net.evaluate(test, test_vals)
     big_results = big_net.evaluate(test, test_vals)
