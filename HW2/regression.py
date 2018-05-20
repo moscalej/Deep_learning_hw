aa532a1031564f5564a8686bc11e7bb028b2091e
Zachary Bamberger <zeb3@cornell.edu>
2018-05-20 19:22:59 +0300

    making validation data sets

--- HW2/regression.py
+++ HW2/regression.py
@@ -27,16 +27,26 @@ def func(x1, x2):
 
 def create_data_sets():
     small = make_points(100)
+    small_validation = make_points(100)
     small_vals = []
+    small_validation_vals = []
     for x in small:
         small_vals.append(func(x[0], x[1]))
+    for x in small_validation:
+        small_validation_vals.append(func(x[0], x[1]))
     small_vals = np.array(small_vals).reshape([100, 1])
+    small_validation_vals = np.array(small_validation_vals).reshape([100, 1])
 
     big = make_points(1000)
+    big_validation = make_points(1000)
     big_vals = []
+    big_validation_vals = []
     for x in big:
         big_vals.append(func(x[0], x[1]))
+    for x in big_validation:
+        big_validation_vals.append(func(x[0], x[1]))
     big_vals = np.array(big_vals).reshape([1000, 1])
+    big_validation_vals = np.array(big_validation_vals).reshape([1000, 1])
 
     test_set = np.linspace(-2, 2, 100)
     test = []
@@ -48,12 +58,34 @@ def create_data_sets():
     test = np.array(test)
     test_vals = np.array(test_vals).reshape([len(test_vals), 1])
 
-    return [small, small_vals, big, big_vals, test, test_vals]
+    result = dict(
+        small=small,
+        small_vals=small_vals,
+        small_validation=small_validation,
+        small_validation_vals=small_validation_vals,
+        big=big,
+        big_vals=big_vals,
+        big_validation=big_validation,
+        big_validation_vals=big_validation_vals,
+        test=test,
+        test_vals=test_vals
+    )
+    return result
 
 
 if __name__ == "__main__":
 
-    small, small_vals, big, big_vals, test, test_vals = create_data_sets()
+    data_sets = create_data_sets()
+    small = data_sets["small"]
+    small_vals = data_sets["small_vals"]
+    small_validation = data_sets["small_validation"]
+    small_validation_vals = data_sets["small_validation_vals"]
+    big = data_sets["big"]
+    big_vals = data_sets["big_vals"]
+    big_validation_vals = data_sets["big_validation_vals"]
+    test = data_sets["test"]
+    test_vals = data_sets["test_vals"]
+
     small_layers = [generate_layer(2, 25, "relu", "l2", 0.4), generate_layer(25, 1, "relu", "l2", 0.4)]
     small_net = mydnn.MyDNN(small_layers, "MSE", 5e-5)
     log_s = small_net.fit(small, small_vals, 1_800, 512, 0.4)
