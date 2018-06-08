734fe281d2d137446a2a1bc6ddade67efc195f09
Alejandro <moscalej@gmail.com>
2018-06-08 14:49:02 +0300

    q3

--- HW3/Code/Preproces.py
+++ HW3/Code/Preproces.py
@@ -23,41 +23,40 @@ IMAGE_CHANNELS = 3
 ######################
 ### PRE PROCESSING ###
 ######################
+def preproces_cfar10():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    # X dimensions should be:
+    #
+    # (<number of samples * number of input channels>,
+    #  <input channel height>,
+    #  <input channel width>,
+    #  <1>)
 
-# X dimensions should be:
-#
-# (<number of samples * number of input channels>,
-#  <input channel height>,
-#  <input channel width>,
-#  <1>)
+    x_train = K.cast_to_floatx(x_train) / 255
+    x_train = x_train.reshape(-1, 32, 32, 3)
 
-x_train = K.cast_to_floatx(x_train) / 255
-x_train = x_train.reshape(-1, 32, 32, 3)
+    x_test = K.cast_to_floatx(x_test) / 255
+    x_test = x_test.reshape(-1, 32, 32, 3)
 
-x_test = K.cast_to_floatx(x_test) / 255
-x_test = x_test.reshape(-1, 32, 32, 3)
+    #     num_train_samples = (x_train.shape[0], 'train samples')
+    #     num_test_samples = (x_test.shape[0], 'test samples')
 
-#     num_train_samples = (x_train.shape[0], 'train samples')
-#     num_test_samples = (x_test.shape[0], 'test samples')
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    def normalize(X_train, X_test):
+        mean = np.mean(X_train, axis=(0, 1, 2, 3))
+        std = np.std(X_train, axis=(0, 1, 2, 3))
+        X_train = (X_train - mean) / (std + 1e-7)
+        X_test = (X_test - mean) / (std + 1e-7)
+        return X_train, X_test
 
+    x_train, x_test = normalize(x_train, x_test)
 
-def normalize(X_train, X_test):
-    mean = np.mean(X_train, axis=(0, 1, 2, 3))
-    std = np.std(X_train, axis=(0, 1, 2, 3))
-    X_train = (X_train - mean) / (std + 1e-7)
-    X_test = (X_test - mean) / (std + 1e-7)
-    return X_train, X_test
-
-
-x_train, x_test = normalize(x_train, x_test)
-
-print(x_train.shape, y_train.shape)
-print(x_test.shape, y_test.shape)
+    print(x_train.shape, y_train.shape)
+    print(x_test.shape, y_test.shape)
+    return x_train, x_test, y_train, y_test
 
 # TODO: fix this
 #     x_train, _, y_train, _ = train_test_split(x_train, y_train, train_size=0.3,
