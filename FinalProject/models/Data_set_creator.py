21f7b217fc222a237bd6ded48ebb1210eabc8528
Alejandro <moscalej@gmail.com>
2018-06-22 17:34:15 +0300

    Add the basics for the final project

---
+++ FinalProject/models/Data_set_creator.py
@@ -0,0 +1,15 @@
+
+
+class DSC:
+    def __init__(self, t_value):
+        """
+
+        """
+    def fit(self,X):
+        """
+        depending of the t_value value it should shred each sample
+        shuffle the picture and get the label
+
+        :param X:
+        :return:
+        """
