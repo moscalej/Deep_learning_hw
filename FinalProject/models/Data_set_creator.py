7fd21bb000e091bd3a6e953fedbd4a3248938991
Alejandro <moscalej@gmail.com>
2018-06-22 17:34:15 +0300

    Add Hw dir
    Signed-off-by: Alejandro <moscalej@gmail.com>

---
+++ FinalProject/models/Data_set_creator.py
@@ -0,0 +1,23 @@
+
+
+class DSC:
+    def __init__(self, t_value):
+        """
+        Black and white
+        """
+        self.data = None
+        self.labels = None
+    def fit(self,X, number_of_combs):
+        """
+        depending of the t_value value it should shred each sample
+        shuffle the picture and get the label
+        X ==> X", Y
+        :param X:
+        :return:
+        """
+
+    def save(self):
+        """
+        save the data set in path
+        :return:
+        """
--- FinalProject/models/shrader_public.py
+++ FinalProject/models/shrader_public.py
@@ -6,17 +6,15 @@ Xa = []
 Xb = []
 y = []
 
-IM_DIR = "/media/yonatan/magnetic/PycharmProjects/DLcourse/images2/"
-OUTPUT_DIR = "output/"
+IM_DIR = r"D:\Ale\Documents\Technion\Deep Learning\DL_HW\FinalProject\data\documents"
+OUTPUT_DIR = r"D:\Ale\Documents\Technion\Deep Learning\DL_HW\FinalProject\data\output\5x5"
 files = os.listdir(IM_DIR)
 
 # update this number for 4X4 crop 2X2 or 5X5 crops.
-tiles_per_dim = 4
-
-
+tiles_per_dim = 5
 
 for f in files:
-    im = cv2.imread(IM_DIR+f)
+    im = cv2.imread(f'{IM_DIR}\\{f}')
     im = cv2.cvtColor(im,cv2.COLOR_RGB2GRAY)
     height = im.shape[0]
     width = im.shape[1]
@@ -27,5 +25,6 @@ for f in files:
         for w in range(tiles_per_dim):
 
             crop = im[h*frac_h:(h+1)*frac_h,w*frac_w:(w+1)*frac_w]
-            cv2.imwrite(OUTPUT_DIR+f[:-4]+"_{}.jpg".format(i),crop)
+            cv2.imwrite(f'{OUTPUT_DIR}\\{f[:-4]}_{i}.jpg',crop)
             i=i+1
+
