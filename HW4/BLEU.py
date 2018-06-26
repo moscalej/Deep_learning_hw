cdb8c2d11d38184fa7fad23470b1d690383668b9
Zachary Bamberger <zeb3@cornell.edu>
2018-06-26 17:08:04 +0300

    returning a list instead of a dictionary

--- HW4/BLEU.py
+++ HW4/BLEU.py
@@ -6,13 +6,13 @@ def calculate_bleu(candidate_sentences, reference_sentences):
     return the mean bleu score for the batch and a dictionary of the bleu scores for each sentence.
     :param candidate_sentences:
     :param reference_sentences:
-    :return: the mean accuracy and a dictionary mapping hashed sentences to their bleu scores.
+    :return: the mean accuracy and a list of the bleu scores of every sentence (in the same order as the sentences).
     """
 
     acc = 0
-    scores = {}
+    scores = []
     for candidate in candidate_sentences:
         score = sentence_bleu(reference_sentences, candidate)
         acc += score
-        scores[candidate] = score
+        scores.append(score)
     return (acc / len(candidate_sentences)), scores
