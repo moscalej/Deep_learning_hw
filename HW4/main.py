382d8bbf43becab332ec38f8e7f5bd1b3d86122f
Alejandro <moscalej@gmail.com>
2018-06-30 19:28:50 +0300

    add nltk to requirements, and divide it by blocks for the run
    Signed-off-by: Alejandro <moscalej@gmail.com>

--- HW4/main.py
+++ HW4/main.py
@@ -12,7 +12,7 @@ from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 #%%
 
 Data, Labels, word_to_id, id_to_word = load_imbd(5_000, 50)
-sentiment = np.ones([Data.shape[0], 50]) * np.reshape(Labels, [Labels.size, 1])
+sentiment = np.multiply(np.ones([Data.shape[0], 50]), Labels.values.reshape([Labels.size, 1]))
 L_rnn = create_labels_rnn(Data)
 a = ReviewGenerator(v_size=5_000, review_len=50, l_s_t_m_state_size=512)
 # %%
@@ -23,13 +23,12 @@ tbCallBack = keras.callbacks.TensorBoard(log_dir=tf_log_path,
                                          histogram_freq=0,  write_graph=True,
                                          write_grads=False, write_images=False, embeddings_freq=0,
                                          embeddings_layer_names=None, embeddings_metadata=None)
-
+a.model.summary()
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.5,
                               patience=1, min_lr=0.00001)
 #%%
 
-a.model.fit([Data, sentiment], L_rnn, 10, 100, validation_split=0.2, verbose=2, callbacks=[tbCallBack],
-            initial_epoch=50)
-# a.model.save('Data/Model_rnn2.h5')
-
+a.model.fit([Data, sentiment], L_rnn, 1024, 200, validation_split=0.2, verbose=1,
+            initial_epoch=0, callbacks=[tbCallBack, checkpoint, reduce_lr])
+a.model.save('Data/Model_rnn2.h5')
