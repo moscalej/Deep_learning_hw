bd1ff6a334b65ddc60af143866535cf7a5840ce1
Alejandro <moscalej@gmail.com>
2018-08-03 11:42:53 +0300

    pasing to gpu debug

--- FinalProject/scripts/vgg_t3.py
+++ FinalProject/scripts/vgg_t3.py
@@ -7,15 +7,6 @@ from models.Data_set_creator import DSC
 from models.dnn import benchmark_model
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 
-    # Determine the right path to the images.
-    # TODO: make this an input to the script later on
-    if "Zach" in os.environ.get('USERNAME'):
-        image_path = r"C:\Users\Zachary Bamberger\Documents\Technion\Deep Learning\Final Project\images"
-        documents_path = r"C:\Users\Zachary Bamberger\Documents\Technion\Deep Learning\Final Project\documents"
-    else:
-        image_path = r"D:\Ale\Documents\Technion\Deep Learning\DL_HW\FinalProject\data\images"
-# Determine the right path to the images.
-# TODO: make this an input to the script later on
 if "Zach" in os.environ.get('USERNAME'):
     image_path = r"C:\Users\Zachary Bamberger\Documents\Technion\Deep Learning\Final Project\images"
     #  TODO define this paths
