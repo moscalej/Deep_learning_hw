cac10f71d854db6dafb9f2459167e0de5d40c07b
Zachary Bamberger <zeb3@cornell.edu>
2018-08-04 21:36:58 +0300

    evaluate function tentatively complete

--- FinalProject/test/evaluate.py
+++ FinalProject/test/evaluate.py
@@ -1,25 +1,48 @@
 import os
 import cv2
+from models.DSC_M import DSCM
+import numpy as np
+import keras
+from models.rnn_clasic import benchmark_model
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 
-def predict(images):
-    labels = []
+
+def predict(model, image):
+
+    labels = model.predict(image)
 
     # here comes your code to predict the labels of the images
     return labels
 
 
+def evaluate(file_dir='example/', model_path=None):
+    """
 
-def evaluate(file_dir='example/'):
+    :param file_dir: the path to a particular image
+    :param model_path: the path to a directory which contains models for various t values (2,4,5)
+    :return: an array of integers representing the correct order of the proper present in the file_dir folder.
+    """
+
     files = os.listdir(file_dir)
     files.sort()
-    images = []
-    for f in files:
-        im = cv2.imread(file_dir + f)
+
+    results = []
+    for index, image in enumerate(files):
+        if index % 100 == 0: print(image)
+        # read image
+        im = cv2.imread(os.path.join(file_dir, image))
+        # convert image to gray scale
         im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
-        images.append(im)
+        im = cv2.resize(im, (96, 96)).reshape([96, 96, 1])
 
+        results.append(im)
 
-    Y = predict(images)
+    image = np.array(results)
+    t = image.shape[0]
+
+    model = keras.models.load_model(os.path.join(model_path, str(t)))
+    Y = model.predict(image)
+
     return Y
 
 
