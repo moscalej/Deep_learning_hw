43762cb76a3738655c20e0faeb05227c3070eb16
Alex Finkelshtein <alex.finkelshtein@intel.com>
2019-01-26 13:45:07 +0200

    some small fixes pocessed2train

--- Final_Project/models/Preprocesses.py
+++ Final_Project/models/Preprocesses.py
@@ -53,7 +53,7 @@ def pre_process_data(input_path: str, cuts: int, shape: int = 32, normalize: boo
                 crop = im[h * frac_h:(h + 1) * frac_h, w * frac_w:(w + 1) * frac_w]
                 crop_rehaped = cv2.resize(crop, (shape, shape))
                 i = i + 1
-                image.append([crop_rehaped, i, number_to_angle(i, cuts), niegbors(i, cuts)])
+                image.append([crop_rehaped, i, number_to_angle(i, cuts), neighbours(i, cuts)])
         images.append(image)
     return np.array(images)
 
@@ -68,23 +68,23 @@ def stich(img1, img2, orient):
     if orient == 1:
         img1_bottom = img1[-2:, :]
         img2_top = img2[0:2, :]
-        stiched = np.rot90(np.concatenate((img1, img2), axis=0))
+        stiched = np.rot90(np.concatenate((img1_bottom, img2_top), axis=0))
     if orient == 2:
         img1_left = img1[:, 0:2]
         img2_right = img2[:, -2:]
-        stiched = np.concatenate((img2, img1), axis=1)
+        stiched = np.concatenate((img1_left, img2_right), axis=1)
     if orient == 3:
         img1_right = img1[:, -2:]
         img2_left = img2[:, 0:2]
-        stiched = np.concatenate((img1, img2), axis=1)
+        stiched = np.concatenate((img1_right, img2_left), axis=1)
     else:
         return "error"
     return stiched
 
 
-def dissimilarity(edge_a, edge_b):
+def similarity(edge_a, edge_b):
     # todo see if reshape needed
-    return float(np.sum(np.sqrt((edge_a - edge_b) ** 2)))
+    return float(-np.sum(np.sqrt((edge_a - edge_b) ** 2)))
 
 
 def choose_false_crops(image, target_crop, options, size):
@@ -98,15 +98,15 @@ def choose_false_crops(image, target_crop, options, size):
     target_edge_left = target_crop[:, 1]
     target_edge_right = target_crop[:, -1]
     for crop_ind in options:
-        crop = image[crop][0]
+        crop = image[crop_ind][0]
         crop_top = crop[1, :]
         crop_down = crop[-1, :]
         crop_left = crop[:, 1]
         crop_right = crop[:, -1]
-        diss_top.append((dissimilarity(target_edge_top, crop_top), crop_ind))
-        diss_down.append((dissimilarity(target_edge_down, crop_down), crop_ind))
-        diss_left.append((dissimilarity(target_edge_left, crop_left), crop_ind))
-        diss_right.append((dissimilarity(target_edge_right, crop_right), crop_ind))
+        diss_top.append((similarity(target_edge_top, crop_top), crop_ind))
+        diss_down.append((similarity(target_edge_down, crop_down), crop_ind))
+        diss_left.append((similarity(target_edge_left, crop_left), crop_ind))
+        diss_right.append((similarity(target_edge_right, crop_right), crop_ind))
     diss_top.sort(key=lambda x: x[0])
     diss_down.sort(key=lambda x: x[0])
     diss_left.sort(key=lambda x: x[0])
@@ -119,6 +119,7 @@ def choose_false_crops(image, target_crop, options, size):
         orient = np.argmax(diss_top[top_ind], diss_down[down_ind], diss_left[left_ind], diss_right[right_ind])
         chosen_crop_ind = combined_list[orient][combined_inds[orient]][1]
         chosen_crop = image[chosen_crop_ind][0]
+        combined_inds[orient] += 1
         chosen_crops.append(chosen_crop, orient)
     return chosen_crops
 
@@ -133,8 +134,9 @@ def processed2train(images: np.ndarray, axis_size) -> list:
         for crop_ind, crop in enumerate(image):
             neighs = crop[3]  # neighbours(up, down, left, right)
             num_neigh = np.count_nonzero(np.array(neighs) + 1)
-            true_crops = [(image[neigh_ind][0], orient) for orient, neigh_ind in neighs if neigh_ind != -1]
-            neigh_inds = [neigh for (neigh, _) in true_crops if neigh != -1]
+            true_crops_inds = [(neigh_ind, orient) for orient, neigh_ind in enumerate(neighs) if neigh_ind != -1]
+            true_crops = [(image[neigh_ind][0], orient) for orient, neigh_ind in enumerate(neighs) if neigh_ind != -1]
+            neigh_inds = [neigh for (neigh, _) in true_crops if true_crops_inds != -1]
             # false_crops = np.random.choice(,
             #                                size=num_neigh)
             false_crops = choose_false_crops(image, crop[0],
