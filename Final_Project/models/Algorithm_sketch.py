981cb8fe9e876902aa61f29b7c9d6be51283e3f4
amoscoso <moscalej@gmail.com>
2019-01-26 22:41:02 +0200

    debugging

--- Final_Project/models/Algorithm_sketch.py
+++ Final_Project/models/Algorithm_sketch.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 import yaml
 from Final_Project.models.Preprocesses import reshape_all
 from keras.models import load_model
+
 clock2orient = {12: 0, 6: 1, 9: 2, 3: 3}
 orient2clock = {value: key for key, value in clock2orient.items()}
 
@@ -21,7 +22,7 @@ class Puzzle:
         self.next_candidates = {first_crop: set([3, 6, 9, 12])}  # keys: puzzle_pieces, values: orientation not used
         self.relative_coo = dict()
         self.relative_coo[first_crop] = (0, 0)  # (right, left)
-        self.neighbours_def = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+        self.neighbours_def = [(0, 1), (-1, 0), (0, -1), (1, 0)]
         self.directions_def = [3, 6, 9, 12]
 
     def add_piece(self, target_crop, new_crop: int, orientation: int) -> None:
@@ -50,11 +51,12 @@ class Puzzle:
             (absX, absY) = self._get_abs_coo(self.relative_coo[new_crop][0], self.relative_coo[new_crop][1])
             self.cyclic_puzzle[absX, absY] = new_crop  # put the piece in the puzzle
             neigh_tups = self._get_neigh(absX, absY)
-            directs = set([3, 6, 9, 12])
-            for (crop_ind, direct) in neigh_tups:
-                directs.remove(direct)
-                self.next_candidates[crop_ind].remove((6 + direct) % 12)  # remove opposite direction
-            self.next_candidates[new_crop] = directs
+            new_directs = set([3, 6, 9, 12])
+            for (crop_ind, direct_) in neigh_tups:  # directions relative to new
+                new_directs.remove(direct_)
+                rem = (6 + direct_) % 12 if direct_ != 6 else 12
+                self.next_candidates[crop_ind].remove(rem)  # remove opposite direction
+            self.next_candidates[new_crop] = new_directs
 
     def get_puzzle_label(self):
         label = []
@@ -68,18 +70,15 @@ class Puzzle:
             rel_col -= 1
         return label
 
-
-
     def _get_neigh(self, absX, absY):
         neighbours = self.neighbours_def
         directions = self.directions_def
-        return [(self.cyclic_puzzle[(absX + dX) % self.axis_size, (absY + dY)% self.axis_size], directions[ind]) for ind, (dX, dY) in enumerate(neighbours)
-                if self.cyclic_puzzle[absX + dX, absY + dY] != -1]
+        return [(self.cyclic_puzzle[self._get_abs_coo(absX + dX, absY + dY)], directions[ind]) for ind, (dX, dY) in
+                enumerate(neighbours)
+                if self.cyclic_puzzle[self._get_abs_coo(absX + dX, absY + dY)] != -1]
 
     def _get_abs_coo(self, relX, relY):
         return (relX % self.axis_size, relY % self.axis_size)
-
-
 
 
 def get_prob_dict(crop_list: list, matcher: Model) -> np.ndarray:
@@ -129,7 +128,7 @@ def fast_fill_mat(predic: np.ndarray, keys: list, results: np.ndarray) -> np.nda
 
 def choose_next(candidates, match_prob_dict):
     best_candidate = (-1, -1, -1)
-    best_candidate_prob = 0
+    best_candidate_prob = 0 - 1e-9
     if candidates == []:  # choose greedy  # todo smart choice
         for candidate_ind in range(match_prob_dict.shape[0]):
             candidate = match_prob_dict[candidate_ind]  # candidate is a matrix num_crops X directions
@@ -139,7 +138,7 @@ def choose_next(candidates, match_prob_dict):
             if mean_match_prob > best_candidate_prob:
                 best_candidate_prob = mean_match_prob
                 best_candidate = (candidate_ind, None, None)
-
+        match_prob_dict[:, best_candidate[0], :] = 0
     else:
         for candidate, clocks in candidates.items():
             candidate_mat = match_prob_dict[candidate, :, :]
@@ -151,6 +150,7 @@ def choose_next(candidates, match_prob_dict):
                 if best_prob > best_candidate_prob:
                     best_candidate_prob = best_prob
                     best_candidate = tuple([candidate, best_crop, orient2clock[orient]])
+            match_prob_dict[:, best_candidate[1], :] = 0
 
     return best_candidate
 
@@ -177,10 +177,10 @@ def assemble(crop_list: list, matcher: Model) -> np.array:
     return puzzle.get_puzzle_label()
 
 
-def predict_2(images ):
-    with open(r'C:\Users\amoscoso\Documents\Technion\deeplearning\Deep_learning_hw\Final_Project\parameters.YAML')as fd: #todo
+def predict_2(images):
+    with open(
+            r'C:\Users\amoscoso\Documents\Technion\deeplearning\Deep_learning_hw\Final_Project\parameters.YAML')as fd:  # todo
         param = yaml.load(fd)
     model = load_model(param['Discri']['path'])
     crops = reshape_all(images, 64)
     return assemble(crops, model)
-
